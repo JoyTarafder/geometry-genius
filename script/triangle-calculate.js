c09497398299576b0b3401eb17977d29aebd4420
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-04-01 12:36:12 +0600

    Fix area calculation and script file paths

---
+++ script/triangle-calculate.js
@@ -0,0 +1,10 @@
+
+function calculateTriangleArea() {
+    const triangleBaseInput = parseFloat(document.getElementById('triangle-base').value);
+
+    const triangleHeightInput = parseFloat(document.getElementById('triangle-height').value);
+
+    document.getElementById('triangle-area').innerText = 0.5 * triangleBaseInput * triangleHeightInput;
+
+    document.getElementById('Area calculation').innerText = `Area of a triangle = ${0.5 * triangleBaseInput * triangleHeightInput} cm²`;
+}
