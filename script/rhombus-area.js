888ff6d7f2604a84579124b23e57a267059e254b
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-04-01 18:54:04 +0600

    Fix parsing issue with rhombus base and height inputs

--- script/rhombus-area.js
+++ script/rhombus-area.js
@@ -1,6 +1,6 @@
 function calculateRhombusArea(){
-    const rhombusBase = document.getElementById('rhombus-base').value;
-    const rhombusHeight = document.getElementById('rhombus-height').value;
+    const rhombusBase = parseFloat(document.getElementById('rhombus-base').value);
+    const rhombusHeight = parseFloat(document.getElementById('rhombus-height').value);
     document.getElementById('rhombus-area').innerText = rhombusBase * rhombusHeight;
     document.getElementById('Area-Calculation').innerText = `Area of a rhombus = ${rhombusBase * rhombusHeight} cm²`;
 
