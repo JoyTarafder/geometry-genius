60e4399eca233d30aafe44d81bb3654dcdf057b7
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-04-01 16:47:59 +0600

    Update ellipse area calculation precision

--- script/ellipse-area.js
+++ script/ellipse-area.js
@@ -1,6 +1,6 @@
 function calculateEllipseArea() {
   const ellipseMajorRadius = parseFloat(document.getElementById("ellipse-major-radius").value);
   const ellipseMinorRadius = parseFloat(document.getElementById("ellipse-minor-radius").value);
-  document.getElementById("ellipse-area").innerText = Math.PI.toFixed(2) * ellipseMajorRadius * ellipseMinorRadius;
-    document.getElementById("Area-Calculation").innerText = `Area of an ellipse = ${Math.PI.toFixed(2) * ellipseMajorRadius * ellipseMinorRadius} cm²`;
+  document.getElementById("ellipse-area").innerText = Math.PI.toFixed(3) * ellipseMajorRadius * ellipseMinorRadius;
+    document.getElementById("Area-Calculation").innerText = `Area of an ellipse = ${Math.PI.toFixed(3) * ellipseMajorRadius * ellipseMinorRadius} cm²`;
 }
