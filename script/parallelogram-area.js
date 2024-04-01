c76b0c5278341b12a4cb957f7b7b20849e2e8723
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-04-01 12:53:00 +0600

    Fix formatting in parallelogram area calculation

---
+++ script/parallelogram-area.js
@@ -0,0 +1,7 @@
+function calculateParallelogramArea() {
+    const parallelogramBase = parseFloat(document.getElementById('parallelogram-base').value);
+    const parallelogramHeight = parseFloat(document.getElementById('parallelogram-height').value);
+    document.getElementById('parallelogram-area').innerText = parallelogramBase * parallelogramHeight;
+
+    document.getElementById('Area calculation').innerText = `Area of a parallelogram = ${parallelogramBase * parallelogramHeight}`;
+}
